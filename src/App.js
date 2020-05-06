372aee5ed1a4ef271b5cc3ac1b16ea9b424dfeb3
Sonia Tejero Sanchez <Shon@Sonias-MacBook-Pro.local>
2020-05-07 01:27:43 +0200

    added menu links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
+import Home from './Home';
+import Post from './Post';
+import Ads from './Ads';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-<Navbar></Navbar>
+    <div className="navBarContainer">
+
+<Router>
+                
+                <Link to="/">Home</Link>
+                <Link to="/post">Post</Link>
+                <Link to ="/ads">Ads</Link>
+
+                <Route exact path="/home" component={Home} />
+                <Route path="/post" component={Post} />
+                <Route path="/ads" component={Ads} />
+            </Router>
+
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+<p>This is post</p>
+    </div>
+  );
+}
+
+export default App;
