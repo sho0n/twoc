7bddac7609cea547d43722c00f04ef5e6474f646
Keivon <keivonproctor@hotmail.com>
2020-05-22 00:03:06 +0100

    Add form and connected server

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 
 function Posts() {
+
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = async data => {
+     console.log(data);
+
+     try {
+      const body = data ;
+      const response = await fetch("https://twoc-nv25lzr5wa-ew.a.run.app/ads", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+    } catch (err) {
+      console.error(err.message);
+    }
+
+
+  }
+  console.log(errors);
+
   return (
     <div className='page'>
-        <p>
-          
-        </p>
+      <div className='form'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+      <input type="text" placeholder="Name" name="name" ref={register({required: true, maxLength: 80})} />
+      <input type="tel" placeholder="Mobile number" name="mobile_number" ref={register({required: true, minLength: 6, maxLength: 13})} />
+      <input type="text" placeholder="Your Red Number" name="red_number" ref={register({required: true, maxLength: 80})} />
+      <input type="text" placeholder="Vehicle Model" name="vehicle_model" ref={register({required: true, maxLength: 80})} />
+      <input type="text" placeholder="Pick up address " name="pick_up_address" ref={register({required: true})} />
+      <input type="text" placeholder="Drop Off Address" name="drop_off_address" ref={register({required: true})} />
+      </div>
+      <textarea name="info" ref={register({required: true})} />
+
+      <input type="submit" />
+    </form>
+    </div>
     </div>
   );
 }
